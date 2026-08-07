ce39e61e168eb2396ebe55bdb25be82ec3f57f90
pen <a206766180@gmail.com>
2026-08-07 15:50:57 +0800

    WIP-登入

---
+++ PaymentData/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HotelManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagementSystem.PaymentData
+{
+    
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlite("Data Source=HotelManagement.db");
+        }
+
+      
+        public DbSet<PaymentModel> Payments { get; set; }
+    }
+}
