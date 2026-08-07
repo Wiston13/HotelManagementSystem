d203cad1349b3c60e7f90a229d71ef6271868331
iris151515 <iris52089@gmail.com>
2026-08-07 17:12:20 +0800

    改髒東西

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,14 +11,15 @@ namespace HotelManagementSystem.Controllers
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-        public IActionResult Payment()
-        {
-            return View();
         }
     }
 }
