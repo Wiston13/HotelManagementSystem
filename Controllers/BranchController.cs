03c392acbe08eab8bb4228acbe2c358a9d992899
YI JEN <youngbeststar520@gmail.com>
2026-08-11 02:04:43 +0800

    feat: 完成分館、房型與實體房間管理頁面
    相關的工作項目: #6

---
+++ Controllers/BranchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementSystem.Controllers
+{
+    public class BranchController : Controller
+    {
+        // 1. 分館管理
+        public IActionResult Branch()
+        {
+            return View(); 
+        }
+
+        // 2. 房型與固定價格管理
+        public IActionResult Price()
+        {
+            return View(); 
+        }
+
+        // 3. 房間管理
+        public IActionResult Rooms()
+        {
+            return View(); 
+        }
+    }
+}
