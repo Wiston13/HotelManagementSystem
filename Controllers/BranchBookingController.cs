93ba34e9d0d0a4abaf01bf8beeeb7e966413c648
aa090800 <a7374239@gmail.com>
2026-08-11 16:36:14 +0800

    更新部分顯示邏輯

---
+++ Controllers/BranchBookingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementSystem.Controllers
+{
+    public class BranchBookingController : Controller
+    {
+        public IActionResult BookingSearch()
+        {
+            return View();
+        }
+        public IActionResult BookingResult()
+        {
+            return View();
+        }
+    }
+}
