3cdf82800badccdc9a09cb7e2415db3e899f5ea3
Wiston13 <wistonlwy@gmail.com>
2026-08-12 16:04:37 +0800

    實作 Check-in 訂單查詢邏輯

--- Controllers/StayController.cs
+++ Controllers/StayController.cs
@@ -1,16 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelManagementSystem.Models;
+using HotelManagementSystem.Models.ViewModels.Stay;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelManagementSystem.Controllers
 {
     public class StayController : Controller
     {
-        public IActionResult CheckIn()
+        private readonly HotelManagementContext _context;
+
+        public StayController(HotelManagementContext context)
         {
-            return View();
+            _context = context;
         }
+
         public IActionResult CheckOut()
         {
             return View();
+        }
+
+        public IActionResult CheckIn(string? bookingNumber)
+        {
+            var model = new CheckInViewModel
+            {
+                BookingNumber = bookingNumber
+            };
+
+            if (string.IsNullOrWhiteSpace(bookingNumber))
+            {
+                return View(model);
+            }
+
+            var booking = _context.Bookings.FirstOrDefault(b => b.BookingNumber == bookingNumber);
+
+            if (booking == null)
+            {
+                model.ErrorMessage = "查無此訂單";
+                return View(model);
+            }
+
+            model.HasResult = true;
+            model.BookerName = booking.BookerName;
+            model.ContactPhone = booking.ContactPhone;
+            model.RoomTypeName = booking.RoomTypeNameSnapshot;
+            model.CheckInDate = booking.CheckInDate;
+            model.CheckOutDate = booking.CheckOutDate;
+            model.BookingStatus = booking.BookingStatus;
+
+            return View(model);
         }
     }
 }
