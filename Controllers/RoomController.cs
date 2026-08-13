1e0ad3fece812ffbc70d49e7ee2cc2e21f387606
youngbeststar520-bit <youngbeststar520@gmail.com>
2026-08-13 15:33:59 +0800

    修改：依據審查規範調整房間管理與控制器參數

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -4,26 +4,28 @@ namespace HotelManagementSystem.Controllers
 {
     public class RoomController : Controller
     {
-        // GET: /Room/ 或 /Room/Index
-        // 顯示房間管理主頁面
-        [HttpGet]
         public IActionResult Index()
         {
-            return View(); // 對應 Views/Room/Index.cshtml
+            return View();
         }
 
-        // POST: /Room/Save
-        // 處理新增或編輯房間資料（樓層為純數字 int）
         [HttpPost]
-        public IActionResult Save(int roomId, string roomNumber, string roomType, int floor, bool isActive)
+        public IActionResult Save(
+            int roomId,
+            int branchId,
+            string roomNumber,
+            int roomTypeId,
+            int floor,
+            string supplyStatus,
+            string? disabledReason)
         {
             if (roomId == 0)
             {
-                // TODO: 執行「新增房間」資料庫邏輯
+                // TODO: 新增房間
             }
             else
             {
-                // TODO: 執行「修改房間」資料庫邏輯
+                // TODO: 修改房間
             }
 
             return RedirectToAction(nameof(Index));
