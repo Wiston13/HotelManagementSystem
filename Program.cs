aa4bb208ce47b04790f02d9982bde380b6b983cb
pen <a206766180@gmail.com>
2026-08-13 16:54:24 +0800

    將 Program 路由回復為原本的預設首頁設定

--- Program.cs
+++ Program.cs
@@ -27,8 +27,9 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-       name: "default",
-    pattern: "{controller=Staff}/{action=Employees}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
 
 app.Run();
