4fc0e8c12a9f831a7769946fbcbf29bc6761f801
pen29 <a206766180@gmail.com>
2026-08-17 19:10:39 +0800

    feat: 新增 E 模組後台靜態頁面 (#15)
    - 新增內部人員登入靜態頁
    - 新增分館員工帳號管理頁
    - 新增訂單資料匯出與唯讀預覽頁
    - 新增基本操作紀錄查詢頁
    - 新增對應頁面 Controller

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementSystem.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Employees()
+        {
+            return View();
+        }
+    }
+}
