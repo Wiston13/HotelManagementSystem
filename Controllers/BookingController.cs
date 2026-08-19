b8f0b9f814919fa15eb9c882955f8a807c2eecf3
aa090800 <a7374239@gmail.com>
2026-08-19 17:19:15 +0800

    WIP: booking/lookup前端修整中

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using HotelManagementSystem.Services;
 using HotelManagementSystem.Models;
 using HotelManagementSystem.Models.BookingSearchModel;
 
@@ -8,11 +9,14 @@ namespace HotelManagementSystem.Controllers
     public class BookingController : Controller
     {
         private readonly HotelManagementContext _context;
+        private readonly TaipeiClock _Clock;
 
-        public BookingController(HotelManagementContext context)
+        public BookingController(HotelManagementContext context,TaipeiClock clock)
         {
             _context = context;
+            _Clock = clock;
         }
+        
 
         [HttpGet]
         public IActionResult RoomSelection(string branchName, DateTime checkIn, DateTime checkOut, int guests)
@@ -38,12 +42,54 @@ namespace HotelManagementSystem.Controllers
             return View();
         }
 
-        [HttpPost]
+        [HttpGet]
         public async Task<IActionResult> Lookup(string BookingNum,string Phone)
         {
+            var model = new BookingData
+            {
+                BookingNum = BookingNum
+            };
 
+            if (string.IsNullOrWhiteSpace(BookingNum))
+            {
+                return View(model);
+            }
 
-            return View();
+            var booking = _context.Bookings.FirstOrDefault(b => b.BookingNumber == BookingNum);
+
+            if (booking == null)
+            {               
+                return View();
+            }
+
+            if (booking.BookingStatus != "Paid")
+            {                
+                return View();
+            }
+
+            var checkInStart = booking.CheckInDate.ToDateTime(new TimeOnly(16, 0));
+            var checkOutDeadline = booking.CheckOutDate.ToDateTime(new TimeOnly(12, 0));
+            var now = _Clock.Now;
+
+            if (now < checkInStart)
+            {
+                return View();
+            }
+
+            if (now >= checkOutDeadline)
+            {
+                return View();
+            }
+
+            var hasStayRecord = _context.StayRecords.Any(s => s.BookingNumber == booking.BookingNumber);
+
+            if (hasStayRecord)
+            {
+                return View();
+            }          
+
+            return View(model);
+
         }
     }
 }
