54cb56c29e4b278c4b934dde0db44da0333c4f43
youngbeststar520-bit <youngbeststar520@gmail.com>
2026-08-24 16:31:25 +0800

    串接房型與固定價格管理後端 CRUD 及動態選單

--- Controllers/RoomTypeController.cs
+++ Controllers/RoomTypeController.cs
@@ -1,35 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelManagementSystem.Models; 
+using HotelManagementSystem.Models.Entities;
 
 namespace HotelManagementSystem.Controllers
 {
     public class RoomTypeController : Controller
     {
-        public IActionResult Index()
+        private readonly HotelManagementContext _context;
+
+        // 1. 注入資料庫
+        public RoomTypeController(HotelManagementContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            // 2. 撈出分館清單 (給畫面上方的下拉選單用)
+            ViewBag.Branches = await _context.Branches.ToListAsync();
+
+            // 3. 撈出所有房型資料 (給下方的表格用)
+            var roomTypes = await _context.RoomTypes.ToListAsync();
+            return View(roomTypes);
         }
 
         [HttpPost]
-        public IActionResult Save(
-            int roomTypeId,
-            int branchId,
-            string roomTypeName,
-            int maxOccupancy,
-            string bedType,
-            decimal nightlyPrice,
-            bool isActive,
-            string imageUrl,
-            string? description)
+        [ValidateAntiForgeryToken]
+        // 4. 把落落長的參數改成直接接收 RoomType 物件
+        public async Task<IActionResult> Save(RoomType model)
         {
-            if (roomTypeId == 0)
+            if (model.RoomTypeId == 0)
             {
-                // TODO: 新增房型
+                // 新增房型
+                _context.RoomTypes.Add(model);
             }
             else
             {
-                // TODO: 修改房型
+                // 修改房型
+                _context.RoomTypes.Update(model);
             }
 
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
     }
