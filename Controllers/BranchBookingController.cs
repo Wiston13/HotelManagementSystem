cd790e24c506ed70d1a6353622715c72f9a0eb1b
aa090800 <a7374239@gmail.com>
2026-09-07 14:25:29 +0800

    修正分館訂單查詢時，資料庫同步順序錯誤

--- Controllers/BranchBookingController.cs
+++ Controllers/BranchBookingController.cs
@@ -78,7 +78,20 @@ namespace HotelManagementSystem.Controllers
                 query = query.Where(x => x.BookingStatus == GetBookingStatusCode(bookingStatus));
             }
 
-            bookingData = await query.Select(x => new BookingData
+            var rawBookingData = await query.Select(x => new
+            {
+                x.BookingNumber,
+                x.CreatedAt,
+                x.BookerName,
+                x.ContactPhone,
+                x.RoomTypeNameSnapshot,
+                x.BookingStatus,
+                StartDate = new DateTime(x.CheckInDate.Year, x.CheckInDate.Month, x.CheckInDate.Day),
+                EndDate = new DateTime(x.CheckOutDate.Year, x.CheckOutDate.Month, x.CheckOutDate.Day),
+                x.TotalAmount
+            }).ToListAsync();
+
+            bookingData = rawBookingData.Select(x => new BookingData
             {
                 BookingNum = x.BookingNumber,
                 BookingDate = x.CreatedAt,
@@ -86,15 +99,14 @@ namespace HotelManagementSystem.Controllers
                 Phone = x.ContactPhone,
                 Roomtype = x.RoomTypeNameSnapshot,
                 BookingStatus = x.BookingStatus,
-                StartDate = new DateTime(x.CheckInDate.Year, x.CheckInDate.Month, x.CheckInDate.Day),
-                EndDate = new DateTime(x.CheckOutDate.Year, x.CheckOutDate.Month, x.CheckOutDate.Day),
-                Price = "NT$ " + x.TotalAmount.ToString()
-            }).ToListAsync();
+                StartDate = x.StartDate,
+                EndDate = x.EndDate,
+                Price = "NT$ " + x.TotalAmount.ToString("#,##0.##", System.Globalization.CultureInfo.GetCultureInfo("zh-TW"))
+            }).ToList();
 
             foreach (var b in bookingData)
             {
                 b.BookingStatus = StatusDisplayHelper.GetBookingStatusText(b.BookingStatus);
-                b.Price =  b.Price). ("#,##0.##", System.Globalization.CultureInfo.GetCultureInfo("zh-TW"));
             }
 
             return View(bookingData);
